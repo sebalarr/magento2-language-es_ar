00a38f19371c001d0f0684b9a172886a090c8044
Sebastián Larrondo <34161382+sebalarr@users.noreply.github.com>
2018-02-04 23:06:06 -0300

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Magento 2 Spanish (Argentina) language pack
+ *
+ * @category   Language
+ * @package    es_AR
+ * @license    Propietary
+ * @author     Cuasar (https://www.cuasar.com.ar)
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'cuasar_es_ar',
+    __DIR__
+);
